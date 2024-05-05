69206249f3414d49f693b174ab252fb12291ab64
cuzik <cuzikjack@gmail.com>
2024-05-05 09:32:44 -0300

    initial commit

---
+++ lib/correios/correios_rest.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "correios_rest/version"
+
+module CorreiosRest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/correios/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Correios
+  module Rest
+    VERSION = "0.1.0"
+  end
+end
